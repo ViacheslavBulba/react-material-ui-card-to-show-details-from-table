488e3e81da4ae190cc5d22e058b721e6cbdf7c36
Slava Bulba <megadevices@gmail.com>
2023-02-16 00:36:47 -0800

    change factors cards alignment

--- src/components/Factors/Factors.js
+++ src/components/Factors/Factors.js
@@ -14,7 +14,7 @@ const Factors = props => {
                 {props.title}
             </Typography>
             <br />
-            <Grid container direction="row" justifyContent="center" alignItems="center" spacing={2} aria-label="factors table">
+            <Grid container direction="row" justifyContent="center" alignItems="flex-start" spacing={2} aria-label="factors table">
                 {props.data.sort((a, b) => (b.weight.value - a.weight.value) * sortOrder).map(item => (
                     <Grid item key={item.name}>
                         <Card variant="outlined" sx={{ width: 250 }}>
